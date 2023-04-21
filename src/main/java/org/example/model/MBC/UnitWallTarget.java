9f708ffad05ccd871dcf825a1087036593f08d4b
mobina <mobina.rashidi1382@outlook.com>
2023-04-22 00:21:55 +0430

    add unitWallTaget methods and enum

--- src/main/java/org/example/model/MBC/UnitWallTarget.java
+++ src/main/java/org/example/model/MBC/UnitWallTarget.java
@@ -1,6 +1,68 @@
 package org.example.model.MBC;
 
 import org.example.model.MBC.Soldier;
+import org.example.model.enums.UnitWallTargetName;
 
 public class UnitWallTarget extends Soldier {
+    public UnitWallTargetName name;
+    public int maxMove;
+    public int speed;
+    public int attackingPower;
+    public int deffense;
+
+    private void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    private void setattackingPower(int attackingPower){
+        this.attackingPower = attackingPower;
+    }
+
+    private void setDeffense(int deffense){
+        this.deffense = deffense;
+    }
+
+    private int getSpeed(){
+        return this.speed;
+    }
+
+    private int getAttackingPower(){
+        return this.attackingPower;
+    }
+
+    private int getDeffense(){
+        return this.deffense;
+    }
+    public void setName(UnitWallTargetName name) {
+        this.name = name;
+    }
+
+    public UnitWallTargetName getName() {
+        return name;
+    }
+
+    private void spearmen(){
+        this.name = UnitWallTargetName.SPEARMEN;
+        this.maxMove = 2;
+    }
+
+    private void macemen(){
+        this.name = UnitWallTargetName.MACEMEN;
+        this.maxMove = 2;
+    }
+
+    private void tunneler(){
+        this.name = UnitWallTargetName.TUNNELER;
+        this.maxMove = 3;
+    }
+
+    private void laddermen(){
+        this.name = UnitWallTargetName.LADDERMEN;
+        this.maxMove = 3;
+    }
+
+    private void assassins(){
+        this.name = UnitWallTargetName.ASSASSINS;
+        this.maxMove = 2;
+    }
 }
