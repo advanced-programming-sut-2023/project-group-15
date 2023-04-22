1898ceb942d535ee421a8cb10e0d0f5f8403a7fc
MahdiXak <mahdiamcfc@gmail.com>
2023-04-22 11:24:26 +0430

    adding something to login menu

--- src/main/java/org/example/view/LoginMenu.java
+++ src/main/java/org/example/view/LoginMenu.java
@@ -4,11 +4,13 @@ import org.example.InputScanner;
 import org.example.controller.LoginMenuController;
 import org.example.controller.ProfileMenuController;
 import org.example.view.enums.commands.LoginMenuEnum;
+import org.example.view.enums.commands.ProfileMenuEnum;
 import org.example.view.enums.outputs.LoginMenuOutput;
+import org.example.view.enums.outputs.ProfileMenuOutput;
 
 import java.util.regex.Matcher;
 
-public class LoginMenu {
+public class LoginMenu extends MainMenu {
     private final LoginMenuController loginMenuController = new ProfileMenuController();
 
     public void run(InputScanner loginMenuScanner) {
@@ -16,9 +18,32 @@ public class LoginMenu {
         String userInput;
         while (true) {
             userInput = loginMenuScanner.getScanner().nextLine();
+            ProfileMenu profileMenu = new ProfileMenu(loginMenuController);
             if (userInput.matches(LoginMenuEnum.USER_LOGOUT.getRegex()))
                 return;
-            //TODO: completing login menu...
+            else if ((loginMenuMatcher = ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.CHANGE_PROFILE_USERNAME))!=null) {
+                profileMenu.changeUserUsername(loginMenuMatcher);
+            } else if ((loginMenuMatcher = ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.CHANGE_PASSWORD))!=null) {
+                profileMenu.changeUserPassword(loginMenuMatcher);
+            } else if ((loginMenuMatcher = ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.CHANGE_PROFILE_EMAIL))!=null) {
+                profileMenu.changeUserEmail(loginMenuMatcher);
+            } else if ((loginMenuMatcher = ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.CHANGE_PROFILE_NICKNAME))!=null) {
+                profileMenu.changeUserNickname(loginMenuMatcher);
+            } else if ((loginMenuMatcher = ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.CHANGE_SLOGAN))!=null) {
+                profileMenu.changeUserSlogan(loginMenuMatcher);
+            } else if (ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.REMOVE_SLOGAN)!=null) {
+                profileMenu.removeUserSlogan();
+            } else if (ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.DISPLAY_USER_PROFILE)!=null) {
+                profileMenu.displayUserProfile();
+            } else if (ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.DISPLAY_USER_SLOGAN)!=null) {
+                profileMenu.displayUserSlogan();
+            } else if (ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.DISPLAY_USER_RANK)!=null) {
+                profileMenu.displayUserRank();
+            } else if (ProfileMenuEnum.getMatcher(userInput,ProfileMenuEnum.DISPLAY_PROFILE)!=null) {
+                profileMenu.displayUserInfo();
+            } else {
+                System.out.println(ProfileMenuOutput.INVALID_COMMAND.getOutput());
+            }
         }
     }
 
