3c84a0d053c53a0b27370b7b6ffce5a65fe75ee7
Raya <rayarezaie83@gmail.com>
2023-04-28 21:06:28 +0430

    map environment class added

--- src/main/java/org/example/controller/GameMenuController.java
+++ src/main/java/org/example/controller/GameMenuController.java
@@ -35,38 +35,6 @@ public class GameMenuController {
     public int popularity(int rate){
         return 1;
     }
-        private void setTexture(int x , int y,String type)
-        {
-
-        }
-        private void setTexture(int x1, int y1,int x2, int y2,String type)
-        {
-
-        }
-        private void clear(int x , int y)
-        {
-            Tile currentTile = Map.findATile(x,y);
-            currentTile.setBuilding(null);
-            currentTile.setTreeName(null);
-            currentTile.setSoldier(null);
-            currentTile.setRock(false);
-        }
-        private void dropUnit(int x , int y, String type)
-        {
-
-        }
-        private void dropBuilding(int x , int y)
-        {
-
-        }
-        private void dropTree(int x , int y, String type)
-        {
-
-        }
-        private void dropRock(int x , int y , char direction)
-        {
-
-        }
 
 
     }
