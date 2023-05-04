4f87ede634abdaa98de9eb904385feab34e98bc4
Raya <rayarezaie83@gmail.com>
2023-05-05 02:46:11 +0430

    map templates added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import org.example.model.enums.LandType;
+import org.example.model.enums.WaterArea;
 import org.example.model.gameData.Map;
+import org.example.model.gameData.MapTemplates;
 import org.example.view.GameMenu;
 import org.example.view.MainMenu;
 import org.example.view.MapMenu;
@@ -8,18 +11,25 @@ import org.example.view.MapMenu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the game!");
-        /*new GameMenu().run();
-
-        new MainMenu().run();*/
+        Map.setCurrentMap(400);
+        for (int i = 10; i < 15; i++){
+            for(int j=60 ; j<65 ;j++)
+            {
+                Map.getCurrentMap()[i][j].setLandType(LandType.RIVER);
+            }
+        }
+         MapTemplates mapTemplates = new MapTemplates();
+        mapTemplates.setMapTemplate1();
+        mapTemplates.setJasonFile1();
+        mapTemplates.setMapTemplate2();
+        mapTemplates.setJasonFile2();
+        mapTemplates.setMapTemplate3();
+        mapTemplates.setJasonFile3();
+        mapTemplates.setMapTemplate4();
+        mapTemplates.setJasonFile4();
+        new GameMenu().run();
+        //new MainMenu().run();
         //Map.setCurrentMap();
-        new MapMenu().run(0,0);
-        new MainMenu().run();
-
-        //Map.setCurrentMap();
-        //dropBuildingController controller = new dropBuildingController();
-       // controller.dropProductiveBuilding(0 , 0 , MILL);
-       // new mapMenu().run();
-        //System.out.println(map.findATile(0 , 0).getLandType());
-       //System.out.println(map.getCurrentMap()[0][0].getBuilding().getName());
+       // new MapMenu().run(0,0);
     }
 }
--- src/main/java/org/example/view/MapMenu.java
+++ src/main/java/org/example/view/MapMenu.java
@@ -4,6 +4,7 @@ import org.example.InputScanner;
 import org.example.controller.MapMenuController;
 import org.example.view.enums.commands.MapEnum;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 
 public class MapMenu {
@@ -11,29 +12,23 @@ public class MapMenu {
     private int yStart;
 
     public void run(Matcher matcher) {
+        int x = 0, y = 0;
+        MapMenuController controller = new MapMenuController();
+        controller.setStartingPoint(matcher);
+        x = controller.getxStart();
+        y = controller.getyStart();
         Matcher mapMenuMatcher;
         String command;
         InputScanner scanner = new InputScanner();
-        int x = 0, y = 0;
-        MapMenuController controller = new MapMenuController();
-        for (int i = 0; i < matcher.groupCount(); i++) {
-            if (matcher.group(i) == null)
-                continue;
-            if (matcher.group(i).equals("x"))
-                x = Integer.parseInt(matcher.group(i + 1));
-            if (matcher.group(i).equals("y"))
-                y = Integer.parseInt(matcher.group(i + 1));
-        }
         controller.showMap(x, y);
         while (true) {
             command = scanner.getScanner().nextLine();
             if (command.matches("\\s*exit\\s*"))
                 break;
-            else if ((mapMenuMatcher = MapEnum.getMatcher(command, MapEnum.MAP_MOVING)) != null) {
+            else if ((mapMenuMatcher = MapEnum.getMatcher(command, MapEnum.MAP_MOVING)) != null)
                 controller.moving(mapMenuMatcher);
-            }
-           // else if ((mapMenuMatcher = MapEnum.getMatcher(command, MapEnum.MAP_DETAILS)) != null)
-             //   System.out.println(controller.showDetails(mapMenuMatcher));
+            else if ((mapMenuMatcher = MapEnum.getMatcher(command, MapEnum.MAP_DETAILS)) != null)
+                System.out.println(controller.showDetails(mapMenuMatcher));
                /* System.out.println(controller.showDetails(Integer.parseInt(mapMenuMatcher.group("xCoordinate")),
                         Integer.parseInt(mapMenuMatcher.group("yCoordinate"))));
             /*else if((mapMenuMatcher = MapMenuEnum.getMatcher(command, MapMenuEnum.SHOW_MAP)) != null)
