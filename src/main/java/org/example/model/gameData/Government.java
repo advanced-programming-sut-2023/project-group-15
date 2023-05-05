d33dbbfe5b541b4d0d7b68e5a697305b8ef9fe68
Raya <rayarezaie83@gmail.com>
2023-05-05 10:53:41 +0430

    government foodrate taxrate and popularity updated

--- src/main/java/org/example/model/gameData/Government.java
+++ src/main/java/org/example/model/gameData/Government.java
@@ -3,55 +3,103 @@ package org.example.model.gameData;
 import org.example.model.enums.Products;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
-    public class Government {
-        int popularity;
-        int foodRate;
-        int taxRate;
-        int fearRate;
-        private static ArrayList<Products> foods = new ArrayList<>();
+public class Government {
+        private static int popularity;
+       private static int foodRate;
+       private static  int taxRate;
+        private static int fearRate;
+        private static HashMap<Products , Double> foods = new HashMap<>();
+        public void showPopularityFactors()
+        {
+            System.out.print("1.food\n2.tax\n3.religion\n4.fear");
+        }
 
         public void setFoodRate(int foodRate) {
+            foodRate = foodRate;
+            switch (foodRate)
+            {
+                case(-2):
+                    popularity -= 8;
+                case(-1):
+                    popularity -= 4;
+                case(1):
+                    popularity += 4 ;
+                case(2):
+                    popularity += 8 ;
 
-            //TODO foodrate should be changed based on numbers of food on the foodlist
+            }
         }
 
         public void setPopularity(int popularity) {
-            // TODO : the popularity should be set here by the user
-            this.popularity = popularity;
+            popularity = popularity;
         }
 
-        public void setTaxrate(int taxrate) {
-            // TODO : the taxrate should be
-            this.taxRate = taxrate;
+        public static void setTaxrate(int taxrate) {
+            taxRate = taxrate;
+            switch (taxRate) {
+                case (-3):
+                    popularity += 7;
+                case(-2):
+                    popularity += 5;
+                case(-1):
+                    popularity += 3;
+                case(0):
+                    popularity += 1;
+                case(1):
+                    popularity -= 2;
+                case(2):
+                    popularity -= 4;
+                case(3):
+                    popularity -= 6;
+                case(4):
+                    popularity -= 8;
+                case(5):
+                    popularity -= 12;
+                case(6):
+                    popularity -= 16;
+                case(7):
+                    popularity -= 20;
+                case(8):
+                    popularity -= 24 ;
+
+
+            }
+
         }
 
-        public void setFeaRate(int fearrate) {
+        public void setFearRate(int fearrate) {
 
             this.fearRate = fearrate;
         }
 
-        public void addFood(Products food) {
-            this.foods.add(food);
+        public void addFood(Products food , double  amount) {
+            foods.put(food , amount);
         }
 
-        public static ArrayList<Products> getFoods() {
-            //TODO the food list is going to be shown here
-            return foods;
-        }
-
-        public void showPopularityRates()
-        {
-
-        }
-        public void showPopularity()
-        {
-
-        }
-
-        public int getTaxrate() {
-            //TODO show tax rate here
+        public static int getTaxrate() {
             return taxRate;
         }
+
+    public static int getFoodRate() {
+        return foodRate;
     }
+
+    public static int getPopularity() {
+        return popularity;
+    }
+
+    public static int getTaxRate() {
+        return taxRate;
+    }
+
+    public static int getFearRate() {
+        return fearRate;
+    }
+
+    public static HashMap<Products, Double> getFoods() {
+        return foods;
+    }
+}
