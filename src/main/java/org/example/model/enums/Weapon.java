f149c500b1e3035e3021e581ecace5b289548557
MahdiXak <mahdiamcfc@gmail.com>
2023-05-06 14:57:16 +0430

    adding something to model/enum

--- src/main/java/org/example/model/enums/Weapon.java
+++ src/main/java/org/example/model/enums/Weapon.java
@@ -1,9 +1,16 @@
 package org.example.model.enums;
 
 public enum Weapon {
-    SWORD,
-    SPEAR,
-    ARCHER,
-    ARMOR,
+    SWORD(0),
+    SPEAR(0),
+    ARCHER(0),
+    ARMOR(0),
     ;
+    Weapon(int damage) {
+        this.damage = damage;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    private final int damage ;
 }
