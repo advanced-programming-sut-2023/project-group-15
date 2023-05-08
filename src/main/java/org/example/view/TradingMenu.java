c663ec84ae4909f453d5626b54516b8b010b2a73
Raya <rayarezaie83@gmail.com>
2023-05-08 14:40:48 +0430

    trading methods added

--- src/main/java/org/example/view/TradingMenu.java
+++ src/main/java/org/example/view/TradingMenu.java
@@ -1,5 +1,17 @@
 package org.example.view;
 
 public class TradingMenu {
+        public void sendRequest()
+        {
 
+        }
+        public void acceptTrade()
+        {
+
+        }
+        public String showTradeList()
+        {
+            return null;
+
+        }
 }
