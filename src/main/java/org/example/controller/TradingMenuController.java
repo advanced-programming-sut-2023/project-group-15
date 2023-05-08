db02e39784a452088c04b46e2710e59a90c3607f
Raya <rayarezaie83@gmail.com>
2023-05-08 22:42:10 +0430

    trading menu

---
+++ src/main/java/org/example/controller/TradingMenuController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+
+public class TradingMenuController{
+    public void sendTradeRequest()
+    {
+
+    }
+    public String showTradeList()
+    {
+        return null;
+    }
+    public String showTradeHistory()
+    {
+     return null;
+    }
+    public void acceptRequest()
+    {
+
+    }
+
+}
