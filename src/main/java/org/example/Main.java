379ca6cac85a58ba1cc8db62b608b7694674647b
Raya <rayarezaie83@gmail.com>
2023-06-09 22:54:27 +0430

    debugging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.example.controller.userControllers.MainMenuController;
 public class Main {
 
     public static void main(String[] args) {
+        //this is the push
         System.out.println("Welcome to the game!");
        // new MainMenuController().checkJsonDirectory();
     }
