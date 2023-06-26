dab2dadea047d00a9b9125e801592c64a160b1da
mobina <mobina.rashidi1382@outlook.com>
2023-06-27 00:46:26 +0330

    debuging

--- src/main/java/org/example/view/userView/Market.java
+++ src/main/java/org/example/view/userView/Market.java
@@ -98,7 +98,7 @@ public class Market extends Application implements Initializable {
             HashMap<String, Integer> buyCoin = new HashMap<>(marketController.show3());
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/ResourcesOfMarket.fxml"));
 
-            Image back = new Image(getClass().getResource("/images/0_0img22.png").toString());
+            Image back = new Image(getClass().getResource("/images1/0_0img22.png").toString());
             ImageView backIcon = new ImageView(back);
             backIcon.setLayoutX(26.0);
             backIcon.setLayoutY(114.0);
@@ -109,14 +109,14 @@ public class Market extends Application implements Initializable {
             sellLable.setFont(Font.font("Century",14));
             sellLable.setLayoutX(330.0);
             sellLable.setLayoutY(85);
-            Image button = new Image((getClass().getResource("/images/0_0img47.png").toString()));
+            Image button = new Image((getClass().getResource("/images1/0_0img47.png").toString()));
             ImageView sellItem = new ImageView(button);
             Text buyLable = new Text("buy        " + sellCoin.get(name));
             buyLable.setFont(Font.font("Century",14));
             buyLable.setLayoutX(330.0);
             buyLable.setLayoutY(130.0);
             ImageView buyItem = new ImageView(button);
-            String address = "/images/"+name+".png";
+            String address = "/images1/"+name+".png";
             System.out.println(address);
             Image image2 = new Image((getClass().getResource(address).toString()));
             ImageView imageView = new ImageView(image2);
@@ -208,7 +208,7 @@ public class Market extends Application implements Initializable {
         } catch (Exception e){
             System.out.println(e);
 
-            Image image2 = new Image((getClass().getResource("/images/0_0img47.png").toString()));
+            Image image2 = new Image((getClass().getResource("/images1/0_0img47.png").toString()));
             ImageView imageView = new ImageView(image2);
             imageView.setPreserveRatio(true);
         }
